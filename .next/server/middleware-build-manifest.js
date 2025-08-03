47a5da27f6860b386fc044f6bf029c380e2f658b
OdairGSoares <contatoodairgomes@gmail.com>
2025-08-03 17:48:52 -0300

    correção nos erros de build

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-458fa2fbd8054787.js","static/chunks/4bd1b696-223acfa09453497a.js","static/chunks/5964-bf304178374af7d1.js","static/chunks/main-app-f07f41b88a592ddf.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-458fa2fbd8054787.js","static/chunks/framework-2c9863a08d67ec10.js","static/chunks/main-42f104df3790985c.js","static/chunks/pages/_app-e417c85570e5e1dc.js"],"/_error":["static/chunks/webpack-458fa2fbd8054787.js","static/chunks/framework-2c9863a08d67ec10.js","static/chunks/main-42f104df3790985c.js","static/chunks/pages/_error-817d8cd3b56025e4.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
